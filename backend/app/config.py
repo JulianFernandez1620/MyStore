2c921b95b4690a9aee4baa6039c437f36699dabc
Cristian Rojas <crrojasp@unal.edu.co>
2023-05-29 11:41:35 -0500

    Mejorando el registro

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgresql:1234509876@localhost/test"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgresql:8VEU5ABZS9wm9.MC@localhost/test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
